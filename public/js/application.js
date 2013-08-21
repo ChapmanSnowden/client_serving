16bb34d705f0fe02627d92434d927c4de41e1eb5
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-08-20 21:17:11 -0700

    skill function working

--- public/js/application.js
+++ public/js/application.js
@@ -7,6 +7,22 @@ $(document).ready(function() {
 
   });
 
+  $('.skill_teller').on('click', function() {
+
+    var getSkill= function() {
+      var urlSkill = window.location.toString().split('/');
+      var skill = $(urlSkill).last();
+        return skill[0];
+    };
+
+    var setSkill = function(skill) {
+      $('#skill_holder').text(skill);
+    };
+
+    setSkill(getSkill());
+
+  });
+
 
 
 });
